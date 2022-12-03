a24042d49cd53bebb0cc02ce2ac0c6525d8c77f8
Maurice Makaay <maurice@makaay.nl>
2022-12-04 00:54:06 +0100

    New solution for 2

--- 03/src/bin/part_two.rs
+++ 03/src/bin/part_two.rs
@@ -1,22 +1,23 @@
 use std::io;
 
 fn main() {
-    let rucksacks: Vec<String> = io::stdin().lines().map(Result::unwrap).collect();
-    let groups: Vec<&[String]> = rucksacks.chunks(3).collect();
+    let input = io::read_to_string(io::stdin()).unwrap();
+    let rucksacks: Vec<&str> = input.lines().collect();
+    let groups: Vec<&[&str]> = rucksacks.chunks(3).collect();
     let common_item_per_group: Vec<char> = groups
         .iter()
         .map(|g| {
             g[0].chars()
-                .filter(|c| g[1].contains(*c) && g[2].contains(*c))
+                .filter(|&c| g[1].contains(c) && g[2].contains(c))
                 .next()
                 .unwrap()
         })
         .collect();
     let priority_per_group: Vec<usize> = common_item_per_group
         .iter()
-        .map(|item| match *item {
-            'a'..='z' => ((*item as u8) - b'a' + 1) as usize,
-            'A'..='Z' => ((*item as u8) - b'A' + 27) as usize,
+        .map(|&item| match item {
+            'a'..='z' => ((item as u8) - b'a' + 1) as usize,
+            'A'..='Z' => ((item as u8) - b'A' + 27) as usize,
             _ => panic!("Invalid item encountered"),
         })
         .collect();
