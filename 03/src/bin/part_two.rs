8b38a27110f3cda3d03543c60ae175c546452c2a
Maurice Makaay <maurice@makaay.nl>
2022-12-04 00:40:12 +0100

    New solution for 2

--- 03/src/bin/part_two.rs
+++ 03/src/bin/part_two.rs
@@ -1,37 +1,26 @@
 use std::io;
 
 fn main() {
-    let input = io::read_to_string(io::stdin()).unwrap();
-    let rucksacks = get_rucksacks(&input);
-    let groups = rucksacks.chunks(3).map(|x| (x[0], x[1], x[2]));
-    let priorities = groups
-        .map(get_common_item_in_group)
-        .map(get_score_for_item)
-        .collect::<Vec<usize>>();
-
-    let answer: usize = priorities.iter().sum();
-    println!("{answer:?}");
-}
-
-fn get_rucksacks(input: &String) -> Vec<&[u8]> {
-    input
-        .trim()
-        .split("\n")
-        .map(|sack| sack.as_bytes())
-        .collect::<Vec<&[u8]>>()
-}
-
-fn get_common_item_in_group((sack_a, sack_b, sack_c): (&[u8], &[u8], &[u8])) -> u8 {
-    *(sack_a
+    let rucksacks: Vec<String> = io::stdin().lines().map(Result::unwrap).collect();
+    let groups: Vec<&[String]> = rucksacks.chunks(3).collect();
+    let common_item_per_group: Vec<char> = groups
         .iter()
-        .filter(|&item| sack_b.contains(item) && sack_c.contains(item))
-        .next()
-        .unwrap())
-}
+        .map(|g| {
+            g[0].chars()
+                .filter(|c| g[1].contains(*c) && g[2].contains(*c))
+                .next()
+                .unwrap()
+        })
+        .collect();
+    let priority_per_group: Vec<usize> = common_item_per_group
+        .iter()
+        .map(|item| match *item {
+            'a'..='z' => ((*item as u8) - b'a' + 1) as usize,
+            'A'..='Z' => ((*item as u8) - b'A' + 27) as usize,
+            _ => panic!("Invalid item encountered"),
+        })
+        .collect();
 
-fn get_score_for_item(item: u8) -> usize {
-    match item {
-        b'a'..=b'z' => (item - b'a' + 1) as usize,
-        _ => (item - b'A' + 27) as usize,
-    }
+    let answer: usize = priority_per_group.iter().sum();
+    println!("{answer:?}");
 }
