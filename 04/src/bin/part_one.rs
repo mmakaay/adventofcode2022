43415f481cc36bea883d2a864ff010ca5066ddbd
Maurice Makaay <maurice@makaay.nl>
2022-12-04 12:59:42 +0100

    Slight cleanup

--- 04/src/bin/part_one.rs
+++ 04/src/bin/part_one.rs
@@ -1,10 +1,21 @@
-use std::io;
+use std::io::{read_to_string, stdin};
+use std::ops::RangeInclusive;
 
-type Assignment = (u8, u8);
+type Assignment = RangeInclusive<u8>;
 type AssignmentPair = (Assignment, Assignment);
 
+trait FullOverlap {
+    fn fully_overlaps(&self, other: &Self) -> bool;
+}
+
+impl FullOverlap for Assignment {
+    fn fully_overlaps(&self, other: &Assignment) -> bool {
+        self.contains(&other.end()) && self.contains(&other.start())   
+    }
+}
+
 fn main() {
-    let input = io::read_to_string(io::stdin()).unwrap();
+    let input = read_to_string(stdin()).unwrap();
     let lines: Vec<&str> = input.lines().collect();
     let pairs: Vec<AssignmentPair> = lines
         .iter()
@@ -13,15 +24,12 @@ fn main() {
                 .split([',', '-'])
                 .map(|x| x.parse::<u8>().unwrap())
                 .collect();
-            ((p[0], p[1]), (p[2], p[3]))
+            (p[0]..=p[1], p[2]..=p[3])
         })
         .collect();
     let pairs_with_full_overlap: Vec<&AssignmentPair> = pairs
         .iter()
-        .filter(|(a, b)| {
-            ((a.0..=a.1).contains(&b.0) && (a.0..=a.1).contains(&b.1)) ||
-            ((b.0..=b.1).contains(&a.0) && (b.0..=b.1).contains(&a.1))
-        })
+        .filter(|(a, b)| a.fully_overlaps(&b) || b.fully_overlaps(&a))
         .collect();
 
     let answer = pairs_with_full_overlap.len();
--- 04/src/bin/part_two.rs
+++ 04/src/bin/part_two.rs
@@ -1,10 +1,24 @@
-use std::io;
+use std::io::{read_to_string, stdin};
+use std::ops::RangeInclusive;
 
-type Assignment = (u8, u8);
+type Assignment = RangeInclusive<u8>;
 type AssignmentPair = (Assignment, Assignment);
 
+trait Overlap {
+    fn overlaps_with(&self, other: &Self) -> bool;
+}
+
+impl Overlap for Assignment {
+    fn overlaps_with(&self, other: &Assignment) -> bool {
+        self.contains(&other.end())
+        || self.contains(&other.start())
+        || other.contains(&self.end())
+        || other.contains(&self.start())        
+    }
+}
+
 fn main() {
-    let input = io::read_to_string(io::stdin()).unwrap();
+    let input = read_to_string(stdin()).unwrap();
     let lines: Vec<&str> = input.lines().collect();
     let pairs: Vec<AssignmentPair> = lines
         .iter()
@@ -13,19 +27,14 @@ fn main() {
                 .split([',', '-'])
                 .map(|x| x.parse::<u8>().unwrap())
                 .collect();
-            ((p[0], p[1]), (p[2], p[3]))
+            (p[0]..=p[1], p[2]..=p[3])
         })
         .collect();
-    let pairs_with_any_overlap: Vec<&AssignmentPair> = pairs
+    let pairs_with_full_overlap: Vec<&AssignmentPair> = pairs
         .iter()
-        .filter(|(a, b)| {
-            (b.0..=b.1).contains(&a.0) ||
-            (b.0..=b.1).contains(&a.1) ||
-            (a.0..=a.1).contains(&b.0) ||
-            (a.0..=a.1).contains(&b.1)
-        })
+        .filter(|(a, b)| a.overlaps_with(&b))
         .collect();
 
-    let answer = pairs_with_any_overlap.len();
+    let answer = pairs_with_full_overlap.len();
     println!("{answer}");
 }
